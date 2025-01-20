ac55a4688e379b005d19cc267068dd0cb87e9b6c
CourantLuna <courantluna@gmail.com>
2025-01-20 13:49:30 -0400

    cambio de pash a put el el updateUsuario

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/', usuarioController.getAllUsuarios);
 router.get('/:id', usuarioController.getUsuarioById);
 router.post('/', usuarioController.addUsuario);
-router.patch('/:id', usuarioController.updateUsuario);
+router.put('/:id', usuarioController.updateUsuario);
 router.delete('/:id', usuarioController.deleteUsuario);
 
 module.exports = router;
